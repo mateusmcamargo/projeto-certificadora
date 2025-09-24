a36e4b30895b9813d9f19ecb28dc644e26b72e27
mateus <mateuscamargo2908@gmail.com>
2025-09-24 20:06:04 -0300

    chore: create navbar draft

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,82 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// react and npm
+import { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// firebase
 import { collection, doc, getDocs } from 'firebase/firestore'
 import { database } from './firebase-config'
 
-function App() {
+// components
+import { Navbar } from './components/navbar/Navbar'
 
+// fonts
+import '@fontsource-variable/montserrat';
+
+// style
+import './App.css'
+
+function App() {
+    
     const [users,   setUsers]   = useState([]);
     const [loading, setLoading] = useState(true);
     const [error,   setError]   = useState(null);
 
-    useEffect(() => {
+    // useEffect(() => {
 
-        const fetchUsers = async () => {
-            try {
-                if (!loading) {setLoading(true);}
-                const usersSnap = await getDocs(collection(database, 'users'));
-                const usersData = usersSnap.docs.map(user => ({
-                    id: user.id,
-                    ...user.data()
-                }));
-                setUsers(usersData);
-            }
-            catch(error) {
-                console.error('ERROR FETCHING USERS: ', error);
-                setError(error);
-            }
-            finally {
-                setLoading(false);
-            }
-        }
+    //     const fetchUsers = async () => {
+    //         try {
+    //             if (!loading) {setLoading(true);}
+    //             const usersSnap = await getDocs(collection(database, 'users'));
+    //             const usersData = usersSnap.docs.map(user => ({
+    //                 id: user.id,
+    //                 ...user.data()
+    //             }));
+    //             setUsers(usersData);
+    //         }
+    //         catch(error) {
+    //             console.error('ERROR FETCHING USERS: ', error);
+    //             setError(error);
+    //         }
+    //         finally {
+    //             setLoading(false);
+    //         }
+    //     }
 
-        fetchUsers();
-    }, []);
+    //     fetchUsers();
+    // }, []);
 
-    if (error) {
-        return (
-            <p>ERROR: {error}</p>
-        );
-    }
-    if (loading) {
-        return (
-            <p>loading...</p>
-        );
-    }
+    // if (error) {
+    //     return (
+    //         <p>ERROR: {error}</p>
+    //     );
+    // }
+    // if (loading) {
+    //     return (
+    //         <p>loading...</p>
+    //     );
+    // }
 
     return (
-        <section className='users'>
-            {users.map(user => (
-                <div className='user' key={user.id}>
-                    <p><span>id: </span>{user.id}</p>
-                    <p><span>nome: </span>{user.name}</p>
-                    <p><span>senha: </span>{user.password}</p>
-                    <p><span>data de nascimento: </span>{user.birth?.toDate().toLocaleString('en-GB').slice(0, 10)}</p>
-                </div>
-            ))}
-        </section> 
+        <Router>
+            <div className='app'>
+                <Routes>
+                    {/* <Route path='/extract' element={<Extract/>}/> */}
+                </Routes>
+
+                <Navbar/>
+
+                {/* <section className='users'>
+                    {users.map(user => (
+                        <div className='user' key={user.id}>
+                        <p><span>id: </span>{user.id}</p>
+                        <p><span>nome: </span>{user.name}</p>
+                        <p><span>senha: </span>{user.password}</p>
+                        <p><span>data de nascimento: </span>{user.birth?.toDate().toLocaleString('en-GB').slice(0, 10)}</p>
+                        </div>
+                    ))}
+                </section>  */}
+            </div>
+        </Router>
     )
 }
 
