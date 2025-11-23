1d57e041e999b42b9148e0ce55f884dc9fce4187
mateus <mateuscamargo2908@gmail.com>
2025-11-23 11:08:50 -0300

    started changes on profile page, UNDONE

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -32,61 +32,101 @@ export function Profile() {
     const [selectedDeposit, setSelectedDeposit] = useState({id:""});
 
     return (
-        <section id="profile">
-            <form onSubmit={(e) => {e.preventDefault();}}>
-                <Label>Nome
-                    <Input type="text" value={state.name} onChange={(e) => dispatch({type:'nameChange', payload:{value:e.target.value}})}/>
-                </Label>
-                <Label>Email
-                    <Input type="email" value={state.email} onChange={(e) => dispatch({type:'emailChange', payload:{value:e.target.value}})}/>
-                </Label>
-                <article>
-                    <Font.Subtitle>Cartões</Font.Subtitle>
-                    <section role="edit card" id="editCard">
-                        <Label>ID do cartão
-                            <Input type="text" value={card.id} onChange={(e) => {setCard(p => ({...p, id:e.target.value}))}}/>
-                        </Label>
-                        <Label>Nome do cartão
-                            <Input type="text"value={card.title} onChange={(e) => {setCard(p => ({...p, title:e.target.value}))}}/>
-                        </Label>
-                        <Button.Submit onClick={() => {dispatch({type:'add_card',payload:{card}})}}>Adicionar Cartão (rascunho)</Button.Submit>
-                        <Button.Submit onClick={() => {dispatch({type:'change_card',payload:{card}})}}>Alterar Cartão (rascunho)</Button.Submit>
-                    </section>
-                    <ul>
-                        { state.cards && state.cards.map(card => (<li key={card.id}>
-                            <Button.Option selected={selectedCard.id == card.id} onClick={() => {setSelectedCard({id:card.id}); setCard(card);}}>
-                                {card.title}
-                                <i className='fa-solid fa-xmark' onClick={() => dispatch({type:'delete_card', payload:{id:card.id}})}></i>
-                            </Button.Option>
-                        </li>))}
-                    </ul>
-                </article>
-                <article>
-                    <Font.Subtitle>Depósitos</Font.Subtitle>
-                    <section role="edit deposit" id="editDeposit">
-                        <Label>ID do depósito
-                            <Input type="text" value={deposit.id} onChange={(e) => {setDeposit(p => ({...p, id:e.target.value}))}}/>
-                        </Label>
-                        <Label>Valor do depósito (R$)
-                            <Input type="text"value={deposit.value} onChange={(e) => {setDeposit(p => ({...p, value:e.target.value}))}}/>
-                        </Label>
-                        <Button.Submit onClick={() => {dispatch({type:'add_deposit',payload:{deposit}})}}>Adicionar Depósito (rascunho)</Button.Submit>
-                        <Button.Submit onClick={() => {dispatch({type:'change_deposit',payload:{deposit}})}}>Alterar Depósito (rascunho)</Button.Submit>
-                    </section>
-                    <ul>
-                        { state.deposits && state.deposits.map(deposit => (<li key={deposit.id}>
-                            <Button.Option selected={selectedDeposit.id == deposit.id} onClick={() => {setSelectedDeposit({id:deposit.id}); setDeposit(deposit);}}>
-                                R$ {deposit.value}
-                                <i className='fa-solid fa-xmark' onClick={() => dispatch({type:'delete_deposit', payload:{id:deposit.id}})}></i>
-                            </Button.Option>
-                        </li>))}
-                    </ul>
-                </article>
-                <div>
-                    <NavLink to="/"><Button.Cancel>Cancelar</Button.Cancel></NavLink>
-                    <Button.Submit onClick={() => dispatch({type:'save'})}>Salvar alterações</Button.Submit>
-                </div>
-            </form>
-        </section>
+        <main>
+            <div className='block profile'>
+                
+                <Font.Title>Seu Perfil</Font.Title>
+                
+                <form onSubmit={(e) => {e.preventDefault();}}>
+
+                    <div className='input-block'>
+                        <Font.Subtitle>Dados Pessoais</Font.Subtitle>
+                        <Label>Nome</Label>
+                        <Input
+                            type="text" 
+                            value={state.name}
+                            onChange={(e) => dispatch({type:'nameChange', payload:{value:e.target.value}})}
+                        />
+                    </div>
+
+                    <div className='input-block'>
+                        <Label>Email</Label>
+                        <Input
+                            type="email"
+                            value={state.email} onChange={(e) => dispatch({type:'emailChange', payload:{value:e.target.value}})}
+                        />
+                    </div>
+
+                    <div>
+                        <Font.Subtitle>Cartões</Font.Subtitle>
+                        <section role="edit card" id="editCard">
+
+                            <div className='input-block'>
+                                <Label>ID do cartão</Label>
+                                <Input
+                                    type="text"
+                                    value={card.id}
+                                    onChange={(e) => {setCard(p => ({...p, id:e.target.value}))}}
+                                />
+                            </div>
+
+                            <div className='input-block'>
+                                <Label>Nome do cartão</Label>
+                                <Input
+                                    type="text"value={card.title}
+                                    onChange={(e) => {setCard(p => ({...p, title:e.target.value}))}}
+                                />
+                            </div>
+
+                            <Button.Submit
+                                onClick={() => {dispatch({type:'add_card',payload:{card}})}}
+                            >
+                                <i className='fa-solid fa-plus'></i>
+                                Adicionar Cartão (rascunho)
+                            </Button.Submit>
+                            <Button.Submit
+                                onClick={() => {dispatch({type:'change_card',payload:{card}})}}
+                            >
+                                <i class="fa-solid fa-plus"></i>
+                                Alterar Cartão (rascunho)
+                            </Button.Submit>
+                        </section>
+                        <ul>
+                            { state.cards && state.cards.map(card => (<li key={card.id}>
+                                <Button.Option selected={selectedCard.id == card.id} onClick={() => {setSelectedCard({id:card.id}); setCard(card);}}>
+                                    {card.title}
+                                    <i className='fa-solid fa-xmark' onClick={() => dispatch({type:'delete_card', payload:{id:card.id}})}></i>
+                                </Button.Option>
+                            </li>))}
+                        </ul>
+                    </div>
+                    <article>
+                        <Font.Subtitle>Depósitos</Font.Subtitle>
+                        <section role="edit deposit" id="editDeposit">
+                            <Label>ID do depósito
+                                <Input type="text" value={deposit.id} onChange={(e) => {setDeposit(p => ({...p, id:e.target.value}))}}/>
+                            </Label>
+                            <Label>Valor do depósito (R$)
+                                <Input type="text"value={deposit.value} onChange={(e) => {setDeposit(p => ({...p, value:e.target.value}))}}/>
+                            </Label>
+                            <Button.Submit onClick={() => {dispatch({type:'add_deposit',payload:{deposit}})}}>Adicionar Depósito (rascunho)</Button.Submit>
+                            <Button.Submit onClick={() => {dispatch({type:'change_deposit',payload:{deposit}})}}>Alterar Depósito (rascunho)</Button.Submit>
+                        </section>
+                        <ul>
+                            { state.deposits && state.deposits.map(deposit => (<li key={deposit.id}>
+                                <Button.Option selected={selectedDeposit.id == deposit.id} onClick={() => {setSelectedDeposit({id:deposit.id}); setDeposit(deposit);}}>
+                                    R$ {deposit.value}
+                                    <i className='fa-solid fa-xmark' onClick={() => dispatch({type:'delete_deposit', payload:{id:deposit.id}})}></i>
+                                </Button.Option>
+                            </li>))}
+                        </ul>
+                    </article>
+                    <div>
+                        <NavLink to="/"><Button.Cancel>Cancelar</Button.Cancel></NavLink>
+                        <Button.Submit onClick={() => dispatch({type:'save'})}>Salvar alterações</Button.Submit>
+                    </div>
+                </form>
+            </div>
+        </main>
     );
 }
